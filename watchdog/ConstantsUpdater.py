85a286352b58f512d972db29241833478d8ed328
Cédric Uden <cedric@uden.de>
2021-09-23 22:38:07 +0200

    updates the JSON file instead of overwriting it

--- watchdog/ConstantsUpdater.py
+++ watchdog/ConstantsUpdater.py
@@ -1,5 +1,6 @@
 import json
 import watchdog.logs.Logging as log
+from ConstantsPaths import secrets_json_path, settings_json_path
 
 
 class ConstantsUpdater:
@@ -8,10 +9,19 @@ class ConstantsUpdater:
     def __init__(self):
         pass
 
-    def write_this_dict(self, dictionary):
-        # WIP
-        json_object = json.dumps(dictionary, indent=4)
+    def write_json_file(self, key, value, target_file):
+        with open(target_file, "r") as jsonFile:
+            data = json.load(jsonFile)
 
-        with open("secrets2.json", "w") as outfile:
-            outfile.write(json_object)
-            self._logger.info(f"Updated {outfile}.")
+        data[key] = value
+
+        with open(target_file, "w") as jsonFile:
+            json.dump(data, jsonFile, indent=4)
+
+        self._logger.info(f"Updated {target_file}")
+
+    def write_secrets_json_file(self, key, value):
+        self.write_json_file(key, value, secrets_json_path)
+
+    def write_settings_json_file(self, key, value):
+        self.write_json_file(key, value, settings_json_path)
