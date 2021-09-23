7244adebe8aefd84f88ed6da1446a3cc145c1f25
Cédric Uden <cedric@uden.de>
2021-09-23 22:57:43 +0200

    this should do it to check the validity of the token. not tested yet due to invalid response from oauth server... continuing tomorrow

--- watchdog/queries/AccessTokenUpdater.py
+++ watchdog/queries/AccessTokenUpdater.py
@@ -2,8 +2,8 @@ from watchdog.queries.AccessToken import AccessToken
 from watchdog.queries.HttpRequestExecutor import HttpRequestExecutor as q
 import watchdog.logs.Logging as log
 from watchdog.Constants import SecretConstants, SettingConstants
-
-import datetime
+from watchdog.ConstantsUpdater import ConstantsUpdater
+from watchdog.TimeHandler import get_current_time_string, get_current_time, get_datetime_from_string
 
 
 class AccessTokenUpdater:
@@ -16,12 +16,35 @@ class AccessTokenUpdater:
         self.secrets = SecretConstants()
 
     def needs_to_be_updated(self):
-        # WIP: enter the date checking and log the new token
         if self.token.date == "":
-            self._logger.debug("UpdateAccessToken needs to be updated.")
+            self._logger.debug("`access_token` needs to be created.")
+            self.make_new_access_token_request_and_update_json()
+        elif self.check_if_token_older_than_1_hour():
+            self._logger.debug("`access_token` needs to be refreshed.")
+            self.make_new_access_token_request_and_update_json()
+        else:
+            self._logger.debug("`access_token` is valid.")
 
-            # res = self.get_new_access_token()
-            # self._logger.info(res.text)
+    def make_new_access_token_request_and_update_json(self):
+        res = self.get_new_access_token()
+        if res.ok:
+            self.create_dict_entry_from_http_result(res)
+
+    def create_dict_entry_from_http_result(self, res):
+        current_time = get_current_time_string()
+        dictionary = {
+                "id": res.text['access_token'],
+                "date": current_time
+
+        }
+        self._logger.debug(f"Updating `access_token` JSON at time {current_time}")
+        ConstantsUpdater().write_secrets_json_file("OAuth_Access_Token_Details", dictionary)
+
+    def check_if_token_older_than_1_hour(self):
+        diff_in_seconds = (get_current_time() - get_datetime_from_string(self.token.date)).total_seconds()
+        diff_in_minutes = int(diff_in_seconds / 60)
+        self._logger.debug(f"Last check occurred {diff_in_minutes} minutes ago.")
+        return diff_in_minutes > 55  # add a little buffer to request new token if close to renewal
 
     def get_new_access_token(self):
         data = {
