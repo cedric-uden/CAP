1d952fad7be67b2cba80da50abcdbbc65ad2165f
Cédric Uden <cedric@uden.de>
2021-09-23 19:01:19 +0200

    added module which represents the `access_token` for OAuth

---
+++ watchdog/queries/AccessToken.py
@@ -0,0 +1,12 @@
+from watchdog.Const import SecretConst
+import watchdog.logs.Logging as log
+
+
+class AccessToken:
+    _logger = log.get_logger(__name__)
+
+    def __init__(self):
+        access_token_dict = SecretConst().get_oauth_access_token_dict()
+        self.id = access_token_dict.get("token")
+        self.age = access_token_dict.get("date")
+        self._logger.debug(f"Loaded token '{self.id}' created on date '{self.age}'")
