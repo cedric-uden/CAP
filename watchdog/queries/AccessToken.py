418e61fc3187533ea13eafa4decf0fc7d93bf147
Cédric Uden <cedric@uden.de>
2021-09-23 21:44:17 +0200

    fixed typo

--- watchdog/queries/AccessToken.py
+++ watchdog/queries/AccessToken.py
@@ -7,6 +7,6 @@ class AccessToken:
 
     def __init__(self):
         access_token_dict = SecretConstants().get_oauth_access_token_dict()
-        self.id = access_token_dict.get("token")
-        self.date = access_token_dict.get("id")
+        self.id = access_token_dict.get("id")
+        self.date = access_token_dict.get("date")
         self._logger.debug(f"Loaded token '{self.id}' created on date '{self.date}'")
