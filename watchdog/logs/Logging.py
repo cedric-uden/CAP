4900f492d0ed8601e679a03cfc57196088d03e6c
Cédric Uden <cedric@uden.de>
2021-09-23 18:59:50 +0200

    added logging module

---
+++ watchdog/logs/Logging.py
@@ -0,0 +1,37 @@
+import logging
+
+
+CRITICAL = logging.CRITICAL
+ERROR = logging.ERROR
+WARNING = logging.WARNING
+INFO = logging.INFO
+DEBUG = logging.DEBUG
+
+
+project_prefix = 'cap'
+
+
+def get_logger(name):
+    return logging.getLogger(f"{project_prefix}.{name}")
+
+
+def setup(stream_level=logging.WARNING, file_level=logging.DEBUG, file_postfix=''):
+    logger = logging.getLogger(project_prefix)
+    logger.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(project_prefix + file_postfix + '.log')
+    fh.setLevel(file_level)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(stream_level)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
