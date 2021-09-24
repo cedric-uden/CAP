f95586cf3c7561f831d3c7babc463665d4cd22b3
Cédric Uden <cedric@uden.de>
2021-09-24 12:33:01 +0200

    now persists new-found videos and gives them the tag `NEW` for now

--- watchdog/business/YouTubeInformation.py
+++ watchdog/business/YouTubeInformation.py
@@ -1,5 +1,7 @@
 from watchdog.queries.CheckYouTubeVideoInspector import CheckYouTubeVideoInspector
 from watchdog.queries.MostRecentYouTubeVideos import MostRecentYouTubeVideos
+from watchdog.utils.PersistVideoInformation import PersistVideoInformation
+from watchdog.business.YouTubeVideoStates import YouTubeVideoStates as state
 import watchdog.logs.Logging as log
 
 
@@ -7,13 +9,34 @@ class YouTubeInformation:
 
     _logger = log.get_logger(__name__)
 
+    def __init__(self):
+        self.all_video_ids = set()
+        self.vid_info = PersistVideoInformation().get_video_information()
+
+    def get_info(self):
+        self.get_most_recent_yt_video_ids()
+        self.add_new_videos_to_video_information_set()
+
     def get_most_recent_yt_video_ids(self):
         all_videos = MostRecentYouTubeVideos().get_json()
-        all_video_ids = set()
         for video in all_videos['items']:
             if len(video['contentDetails']) > 0:
-                all_video_ids.add(video['contentDetails']['upload']['videoId'])
+                self.all_video_ids.add(video['contentDetails']['upload']['videoId'])
 
-        self._logger.info(f"Most recent videos on channel: {str(all_video_ids)}")
-        return all_video_ids
+        self._logger.info(f"Most recent videos on channel: {str(self.all_video_ids)}")
+        return self.all_video_ids
+
+    def is_video_in_video_information_set(self, video_id):
+        return True if self.vid_info.get(video_id) else False
+
+    def add_new_videos_to_video_information_set(self):
+        for video in self.all_video_ids:
+            if not self.is_video_in_video_information_set(video):
+                self.vid_info.update({video: state.NEW})
+                self._logger.debug(f"Added video {video} to the information set.")
+            PersistVideoInformation().store_video_information(self.vid_info)
+            else:
+                self._logger.debug(f"Video {video} was found in the video information set.")
+
+
 
---
+++ watchdog/business/YouTubeVideoStates.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class YouTubeVideoStates(Enum):
+    TO_BE_UPLOADED = "TO_BE_UPLOADED"
+    UPLOADED = "UPLOADED"
+    NOT_A_SERMON = "NOT A SERMON"
+    NEW = "NEW"
