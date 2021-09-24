b6a50c870319b7c2d4064ec4b2febdbe4abbd30e
Cédric Uden <cedric@uden.de>
2021-09-24 15:33:57 +0200

    now checks which videos are to be uploaded to FTP server

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,10 @@
 import src.logs.Logging as log
 from src.business.YouTubeInformation import YouTubeInformation
 from src.mp3download.MP3Downloader import MP3Downloader
+from src.ftp_uploader.FTPUploader import FTPUploader
 
 log.setup(stream_level=log.INFO)
 
 # YouTubeInformation().update()
-MP3Downloader().download()
+# MP3Downloader().download()
+FTPUploader().upload()
--- src/ftp_uploader/FTPUploader.py
+++ src/ftp_uploader/FTPUploader.py
@@ -1,5 +1,7 @@
 import src.logs.Logging as log
 from src.constants.Constants import SecretConstants, SettingConstants
+from src.utils.PersistVideoInformation import PersistVideoInformation
+from src.business.YouTubeVideoStates import YouTubeVideoStates as state
 
 import ftplib
 
@@ -10,3 +12,16 @@ class FTPUploader:
     def __init__(self):
         self.ftp_path = SettingConstants().get_ftp_path()
         self.ftp_info = SecretConstants().get_ftp_dict()
+        self.all_videos = PersistVideoInformation().get_video_information()
+        self.videos_to_be_uploaded = set()
+        self.get_files_to_upload()
+
+    def get_files_to_upload(self):
+        for video_id, status in self.all_videos.items():
+            if status == state.DOWNLOADED_NOT_UPLOADED:
+                self.videos_to_be_uploaded.add(video_id)
+                self._logger.debug(f"Found video {video_id} "
+                                   f"to be uploaded to FTP.")
+
+    def upload(self):
+        pass
