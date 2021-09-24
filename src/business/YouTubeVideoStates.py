0296243a21daf8a54e9e16d9a5b374b132e37960
Cédric Uden <cedric@uden.de>
2021-09-24 16:26:48 +0200

    reordered and added comments to the states

--- src/business/YouTubeVideoStates.py
+++ src/business/YouTubeVideoStates.py
@@ -2,8 +2,11 @@ from enum import Enum
 
 
 class YouTubeVideoStates(Enum):
+    # while processing
+    NEW = "NEW"
     TO_BE_DOWNLOADED = "TO_BE_DOWNLOADED"
+    DOWNLOADED_NOT_UPLOADED = "DOWNLOADED_NOT_UPLOADED"
+
+    # final states
     UPLOADED = "UPLOADED"
     NOT_A_SERMON = "NOT A SERMON"
-    NEW = "NEW"
-    DOWNLOADED_NOT_UPLOADED = "DOWNLOADED_NOT_UPLOADED"
