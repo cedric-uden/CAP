ade9180797c54e6ab9ce183b748e26a8cc25e4ac
Cédric Uden <cedric@uden.de>
2021-09-27 18:48:25 +0200

    added a whole bunch of extra symbols to sanitize

--- src/utils/FilenameSanitizer.py
+++ src/utils/FilenameSanitizer.py
@@ -5,12 +5,42 @@ import os
 class FilenameSanitizer:
     _logger = log.get_logger(__name__)
 
-    def clean_filename(self, filepath):
-        new_filepath = self.replace_whitespace(filepath)
-        new_filepath = self.replace_pipe(new_filepath)
-        if filepath != new_filepath:
-            os.rename(filepath, new_filepath)
-            self._logger.debug(f"Renamed {filepath} to {new_filepath}")
+    def clean_filename(self, name):
+        new_name = self.replace_whitespace(name)
+        new_name = self.replace_pipe(new_name)
+        new_name = self.replace_forwardslash(new_name)
+        new_name = self.replace_backslash(new_name)
+        new_name = self.replace_lt(new_name)
+        new_name = self.replace_gt(new_name)
+        new_name = self.replace_hashtag(new_name)
+        if name != new_name:
+            self._logger.debug(f"Suggesting {new_name} instead of {name}")
+            return new_name
+
+    def replace_forwardslash(self, filepath):
+        while "/" in filepath:
+            filepath = filepath.replace("/", "-")
+        return filepath
+
+    def replace_backslash(self, filepath):
+        while "\\" in filepath:
+            filepath = filepath.replace("\\", "-")
+        return filepath
+
+    def replace_lt(self, filepath):
+        while "<" in filepath:
+            filepath = filepath.replace("<", "-")
+        return filepath
+
+    def replace_gt(self, filepath):
+        while ">" in filepath:
+            filepath = filepath.replace(">", "-")
+        return filepath
+
+    def replace_hashtag(self, filepath):
+        while "#" in filepath:
+            filepath = filepath.replace("#", "_")
+        return filepath
 
     def replace_pipe(self, filepath):
         while "|" in filepath:
