b8b548b538ad7ddf04af22ba20aa566bfd80513d
Cédric Uden <cedric@uden.de>
2021-09-28 16:04:40 +0200

    added extension map to HTTP server

--- web/server.py
+++ web/server.py
@@ -3,9 +3,25 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 PORT = 8080
 
 
+class HttpRequestHandler(SimpleHTTPRequestHandler):
+    extensions_map = {
+        '': 'application/octet-stream',
+        '.manifest': 'text/cache-manifest',
+        '.html': 'text/html',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.svg':	'image/svg+xml',
+        '.css':	'text/css',
+        '.js': 'application/x-javascript',
+        '.wasm': 'application/wasm',
+        '.json': 'application/json',
+        '.xml': 'application/xml',
+    }
+
+
 def run():
     server_address = ('', PORT)
-    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd = HTTPServer(server_address, HttpRequestHandler)
     httpd.serve_forever()
 
 
