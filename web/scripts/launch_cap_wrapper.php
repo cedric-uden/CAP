90c47dd5c4026fd812691858775f16f88e8c0956
Cédric Uden <cedric@uden.de>
2021-09-28 20:57:10 +0200

    added php wrapper to launch scripts on server

---
+++ web/scripts/launch_cap_wrapper.php
@@ -0,0 +1,12 @@
+<?php
+// $out =  exec('pwd');
+// echo $out;
+// echo "<br>";
+// $out =  exec('whoami');
+// echo $out;
+
+// make sure the permissions are set to `chown -R www-data:www-data *`
+// inside the `/var/www/html` directory
+
+$out =  exec('./launch_cap.sh');
+?>
