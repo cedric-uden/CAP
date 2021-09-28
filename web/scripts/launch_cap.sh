b6e418b67bc5fc97046eabde5d286c1d5be3f925
Cédric Uden <cedric@uden.de>
2021-09-28 21:35:16 +0200

    now launches the script from directory

--- web/scripts/launch_cap.sh
+++ web/scripts/launch_cap.sh
@@ -1,1 +1,9 @@
-touch foo
+cwd="/home/ced/git/icf/CAP/"
+main="Main.py"
+src="src/"
+python="/home/ced/Apps/miniconda3/envs/cap/bin/python"
+
+
+export PYTHONPATH=$PYTHONPATH:/$cwd$src
+cd $cwd
+eval $python" "$src$main
